01da82756c217123ccecc99370153e0ca1491f5c
LEONG HONG YI <100272517+HLeong12345@users.noreply.github.com>
2023-11-02 01:33:22 +0800

    Paged the blocks into 1000/page

--- interface.py
+++ interface.py
@@ -178,21 +178,35 @@ class ProjectWindow(tk.Tk):
             self.planCanvas.itemconfig(text, font=writingFont)
 
     def destroy_block_content_display(self):
-        if(self.display_block_label is not None):
-            self.display_block_label.destroy()
-            self.display_block_label = None
+            if(self.display_block_label is not None):
+                self.display_block_label.destroy()
+                self.display_block_label = None
 
-        if(self.table is not None):
-            self.table.destroy()
-            self.table = None
+            if(self.table is not None):
+                self.table.destroy()
+                self.table = None
 
-        if(self.v_scroll is not None):
-            self.v_scroll.destroy()
-            self.v_scroll = None
+            if(self.v_scroll is not None):
+                self.v_scroll.destroy()
+                self.v_scroll = None
 
-        if(self.h_scroll is not None):
-            self.h_scroll.destroy()
-            self.h_scroll = None
+            if(self.h_scroll is not None):
+                self.h_scroll.destroy()
+                self.h_scroll = None
+
+    def destroy_block_frame(self):
+        if(self.block_frame_label is not None):
+            self.block_frame_label.destroy()
+            self.block_frame_label = None
+
+        if(self.block_canvas is not None):
+            self.block_canvas.destroy()
+            self.block_canvas = None
+
+        if(self.block_scrollbar is not None):
+            self.block_scrollbar.destroy()
+            self.block_scrollbar = None
+
 
     def destroy_disk_tab(self):
         # Destroy disk_tab
@@ -204,6 +218,9 @@ class ProjectWindow(tk.Tk):
         # Actions to do when block_button is clicked
         self.display_relation(relation, block)
         
+        ## REMEMBER TO ADD INTO CODE IN GITHUB
+        for b in self.block_buttons:
+            b.configure(bg = "grey50")
         #print("click_block_button " + str(block))
         button.configure(bg='#444444')
 
@@ -236,17 +253,17 @@ class ProjectWindow(tk.Tk):
         self.block_frame.grid_columnconfigure(1, weight=1)  
         
         # Block_frame_label on block_frame
-        block_frame_label = tk.Label(self.block_frame, text='Block Options', wraplength=90,  borderwidth=2, relief="groove")
-        block_frame_label.grid(row=0, column=0, sticky="nsew", padx=0, pady=0, columnspan=2)
+        self.block_frame_label = tk.Label(self.block_frame, text='Block Options', wraplength=90,  borderwidth=2, relief="groove")
+        self.block_frame_label.grid(row=0, column=0, sticky="nsew", padx=0, pady=0, columnspan=2)
 
         # Create block_canvas to hold scrollbar and dynamic buttons
         self.block_canvas = tk.Canvas(self.block_frame, bg="white", width = 0, height=200)
         self.block_canvas.grid(row=1, column=0, sticky = "nsew")
         
         # Create and place the scrollbar
-        block_scrollbar = tk.Scrollbar(self.block_frame, orient="vertical", command=self.block_canvas.yview, width = 0)
-        block_scrollbar.grid(row=1, column=1, columnspan = 1, sticky="nsew")
-        self.block_canvas.config(yscrollcommand=block_scrollbar.set)
+        self.block_scrollbar = tk.Scrollbar(self.block_frame, orient="vertical", command=self.block_canvas.yview, width = 0)
+        self.block_scrollbar.grid(row=1, column=1, columnspan = 1, sticky="nsew")
+        self.block_canvas.config(yscrollcommand=self.block_scrollbar.set)
         self.block_canvas.bind('<Configure>', self.on_configure_block_canvas)
 
         # block_canvas_inner_frame to hold dynamic buttons 
@@ -255,6 +272,8 @@ class ProjectWindow(tk.Tk):
 
         # Add dynamic buttons based on the relation clicked
         self.block_buttons = []
+        print("block_IDs")
+        print(block_IDs)
         for block in block_IDs:
             self.add_block_button(relation, block)
         self.block_canvas.configure(scrollregion=self.block_canvas.bbox("all"))
@@ -264,9 +283,15 @@ class ProjectWindow(tk.Tk):
         
         # Clear the content at block_content_frame
         self.destroy_block_content_display()
+        self.destroy_block_frame()
+        ## REMEMBER TO ADD INTO CODE IN GITHUB
+        for b in self.relation_buttons:
+            b.configure(bg = "grey50")
+
 
         # Configure block_frame and add in new buttons
-        self.config_block_frame(relation, block_IDs)
+        # self.config_block_frame(relation, block_IDs)
+        self.config_page_frame(relation, block_IDs)
         button.configure(bg='#444444')
 
     def on_configure_relation_canvas(self, event):
@@ -327,7 +352,7 @@ class ProjectWindow(tk.Tk):
     def display_relation(self, relation, ctid):
         self.destroy_block_content_display()
         # Rerieve the schema and result based on relation and ctid
-        schema, result = execute_block_query(self.connection, relation, ctid)
+        schema, result = execute_block_query(None, relation, ctid)
 
         # Adjust Layout
         self.block_content_frame.grid_rowconfigure(0, weight=1)      
@@ -363,6 +388,83 @@ class ProjectWindow(tk.Tk):
         # Do minor adjustment
         self.block_content_frame.grid_rowconfigure(0, weight=1)
         self.block_content_frame.grid_columnconfigure(0, weight=1)
+        
+    def click_page_button(self, relation, block_IDs, button):
+                # Action to do when relation_button is clicked
+                
+                # Clear the content at block_content_frame
+                self.destroy_block_frame()
+                
+
+                ## REMEMBER TO ADD INTO CODE IN GITHUB
+                for b in self.page_buttons:
+                    b.configure(bg = "grey50")
+
+
+                # Configure block_frame and add in new buttons
+                self.config_block_frame(relation, block_IDs)
+                button.configure(bg='#444444')
+
+    def add_page_button(self,relation, page, counter):
+        # Add block_button to create dynamic buttons into block_canvas
+        page_button = tk.Button(self.page_canvas_inner_frame, text="Page:" + str(counter), bg = "grey50")
+        page_button.configure(command = lambda btn = page_button:self.click_page_button(relation, page, btn))
+
+        # Add hover effect on button
+        # relation_button.bind("<Enter>", on_hover)
+        # relation_button.bind("<Leave>", on_leave)
+        page_button.pack(fill = "both")
+
+        # Adjust scrollbar
+        self.page_buttons.append(page_button)
+        self.page_canvas.update_idletasks()
+        self.page_canvas.bind('<Configure>', self.on_configure_page_canvas)
+
+    def on_configure_page_canvas(self, event):
+        # Configure scroll bar for block_frame
+        if self.page_canvas is not None:
+            self.page_canvas.configure(scrollregion=self.page_canvas.bbox("all"))
+
+    def paginate(self, block_IDs):
+        # Define the size of each sublist
+        sublist_size = 1000
+        # Use a list comprehension to create sublists
+        sublists = [block_IDs[i:i + sublist_size] for i in range(0, len(block_IDs), sublist_size)]
+        return sublists
+
+    def config_page_frame(self, relation, block_IDs):
+        # Adjust layout of block_frame
+        self.page_frame.grid_rowconfigure(0, weight=1)      
+        self.page_frame.grid_rowconfigure(1, weight=9)     
+        self.page_frame.grid_columnconfigure(0, weight=9)  
+        self.page_frame.grid_columnconfigure(1, weight=1)  
+        
+        # Block_frame_label on block_frame
+        page_frame_label = tk.Label(self.page_frame, text='Block Page', wraplength=90,  borderwidth=2, relief="groove")
+        page_frame_label.grid(row=0, column=0, sticky="nsew", padx=0, pady=0, columnspan=2)
+
+        # Create block_canvas to hold scrollbar and dynamic buttons
+        self.page_canvas = tk.Canvas(self.page_frame, bg="white", width = 0, height=200)
+        self.page_canvas.grid(row=1, column=0, sticky = "nsew")
+        
+        # Create and place the scrollbar
+        page_scrollbar = tk.Scrollbar(self.page_frame, orient="vertical", command=self.page_canvas.yview, width = 0)
+        page_scrollbar.grid(row=1, column=1, columnspan = 1, sticky="nsew")
+        self.page_canvas.config(yscrollcommand=page_scrollbar.set)
+        self.page_canvas.bind('<Configure>', self.on_configure_page_canvas)
+
+        # page_canvas_inner_frame to hold dynamic buttons 
+        self.page_canvas_inner_frame = tk.Frame(self.page_canvas)
+        self.page_canvas.create_window((0, 0), window=self.page_canvas_inner_frame, anchor='nw')
+
+        # Add dynamic buttons based on the relation clicked
+        pages = self.paginate(block_IDs)
+        self.page_buttons = []
+        counter = 0
+        for page in pages:
+            self.add_page_button(relation, page, counter)
+            counter+=1
+        self.page_canvas.configure(scrollregion=self.page_canvas.bbox("all"))
         
     def create_QEP_tab(self):
         self.QEP_tab = ttk.Frame(self.notebook)
@@ -417,20 +519,26 @@ class ProjectWindow(tk.Tk):
         self.block_frame = tk.Frame(self.disk_tab, borderwidth=2, relief="groove", bg = "white" )
         self.block_frame.grid_propagate(0)
 
+        # Partition disk_tab into page_frame
+        self.page_frame = tk.Frame(self.disk_tab, borderwidth=2, relief="groove", bg = "white" )
+        self.page_frame.grid_propagate(0)
+
         # Partition disk_tab into block_content_frame
         self.block_content_frame = tk.Frame(self.disk_tab, bg = "white", borderwidth=2, relief="groove")
         self.block_content_frame.grid_propagate(0)
 
         # Adjust layout of disk_tab
         self.disk_tab.grid_columnconfigure(0, weight=2)  
-        self.disk_tab.grid_columnconfigure(1, weight=8)  
+        self.disk_tab.grid_columnconfigure(1, weight=2)  
+        self.disk_tab.grid_columnconfigure(2, weight=6)  
         self.disk_tab.grid_rowconfigure(0, weight=1)      
         self.disk_tab.grid_rowconfigure(1, weight=1)      
 
         # Place relation_frame, block_frame, block_content_frame
         self.relation_frame.grid(row=0, column=0, sticky="nsew")
-        self.block_frame.grid(row=1, column=0, sticky="nsew")
-        self.block_content_frame.grid(row=0, column=1, rowspan=2, sticky="nsew")
+        self.block_frame.grid(row=0, column=1, rowspan = 2, sticky="nsew")
+        self.page_frame.grid(row=1, column=0, sticky="nsew")
+        self.block_content_frame.grid(row=0, column=2, rowspan=2, sticky="nsew")
 
         # Configure relation_frame to display buttons of relation
         self.config_relation_frame(block_id_per_table)
@@ -462,7 +570,11 @@ class ProjectWindow(tk.Tk):
         self.v_scroll = None
         self.h_scroll = None
         self.blocks_frame = None
+        self.block_frame_label = None
         self.block_content_frame = None
+        self.block_scrollbar = None
+        self.block_canvas = None
+        self.block_scrollbar = None
         
         self.block_id_per_table = {}
         self.connection = connect_database(database="TPC-H",password="since2001")
